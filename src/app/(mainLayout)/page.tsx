ba93cffe83862d7461519e01227dae81a22c2b27
Darshan Patel <ddpatel97@gmail.com>
2025-02-24 17:35:53 -0330

    feat: added Dashboard page

--- src/app/(mainLayout)/page.tsx
+++ src/app/(mainLayout)/page.tsx
@@ -1,11 +1,38 @@
-import { Button } from '@/components/ui/button';
+import FinancialInsights from '@/components/Dashboard/FinancialInsights/FinancialInsights';
+import TransactionsInsight from '@/components/Dashboard/TransactionsInsights/TransactionsInsight';
+import QuickStatCard from '@/components/general/QuickStatCard';
+import { monthlyStats } from '../data/mockData';
 
-export default function Home() {
+export default function DashboardHome() {
   return (
-    <div className="">
-      <Button variant="default" className="mx-auto">
-        Hello Home!
-      </Button>
+    <div className="space-y-6">
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+        <QuickStatCard
+          title="Monthly Income"
+          amount={monthlyStats.income}
+          previousAmount={monthlyStats.previousIncome}
+          type="income"
+        />
+        <QuickStatCard
+          title="Monthly Expenses"
+          amount={monthlyStats.expenses}
+          previousAmount={monthlyStats.previousExpenses}
+          type="expense"
+        />
+        <QuickStatCard
+          title="Available Balance"
+          amount={monthlyStats.balance}
+          type="balance"
+        />
+        <QuickStatCard
+          title="Monthly Savings"
+          amount={monthlyStats.savings}
+          percentage={(monthlyStats.savings / monthlyStats.income) * 100}
+          type="savings"
+        />
+      </div>
+      <TransactionsInsight />
+      <FinancialInsights />
     </div>
   );
 }
