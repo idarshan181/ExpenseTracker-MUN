8d623876c06b9e7f8d15985b4275f0a00513baf4
Darshan Patel <ddpatel97@gmail.com>
2025-03-21 14:21:40 -0230

    fix(state): cleared login state

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -1,7 +1,13 @@
 'use server';
 
+import { cookies } from 'next/headers';
 import { signOut } from '../utils/auth';
 
 export const handleSignOut = async () => {
   await signOut({ redirectTo: '/' });
+
+  (await cookies()).delete('next-auth.session-token');
+  (await cookies()).delete('next-auth.csrf-token');
+
+  return { success: true };
 };
