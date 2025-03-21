ddd2beb9e6adabd6d9246ea4109b8dd101b00858
vtpatel <vivekpatel21101@gmail.com>
2025-03-21 18:13:57 -0230

    fix: height, width for login page image

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,7 +7,13 @@ export default function Login() {
     <div className="flex min-h-screen w-screen items-center justify-center ">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <Link href="/" className="flex items-center gap-2 self-center">
-          <Image src="/logos/logo.png" alt="Logo" className="size-10" />
+          <Image
+            src="/logos/logo.png"
+            alt="Logo"
+            width={40} // or whatever size fits your design
+            height={40}
+            className="size-10"
+          />
           <h1 className="text-2xl font-bold">
             ExpenseVision
             {' '}
