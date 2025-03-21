3ccbf5c661a2d7a43f1d9897c4b2ef63407223b1
Darshan Patel <ddpatel97@gmail.com>
2025-03-21 14:34:52 -0230

    fix: client side logging out

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -1,21 +1,7 @@
-/* eslint-disable no-console */
 'use server';
 
-import { cookies } from 'next/headers';
 import { signOut } from '../utils/auth';
 
 export const handleSignOut = async () => {
-  console.log('inside handleSignOut');
   await signOut({ redirectTo: '/' });
-
-  (await cookies()).delete('__Secure-authjs.session-token');
-  (await cookies()).delete('authjs.session-token');
-  (await cookies()).delete('authjs.callback-url');
-  (await cookies()).delete('__Secure-authjs.state');
-  (await cookies()).delete('__Host-authjs.csrf-token');
-  (await cookies()).delete('authjs.csrf-token');
-
-  console.log((await cookies()).getAll());
-
-  return { success: true };
 };
