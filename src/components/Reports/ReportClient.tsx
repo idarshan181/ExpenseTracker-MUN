ee5490052efc4cf44db6be1ce6651ba86a0a349e
Darshan Patel <ddpatel97@gmail.com>
2025-03-22 17:04:48 -0230

    feat: reusable quickstat card and report clients

--- src/components/Reports/ReportClient.tsx
+++ src/components/Reports/ReportClient.tsx
@@ -1,25 +1,11 @@
-/* eslint-disable no-console */
 'use client';
 
-import { useMonthlySummary, useSpendingByCategories, useTopSpendingByCategory } from '@/hooks/useAnalytics';
 import ExpenseDistribution from './ExpenseDistribution';
 import IncomeExpenseTrend from './IncomeExpenseTrend';
 import Summary from './Summary';
 import TopSpendings from './TopSpendings';
 
 export default function ReportClient() {
-  const { data: spendingCategories, isLoading: isSpendingLoading } = useSpendingByCategories();
-  const { data: topSpending, isLoading: isTopSpendingLoading } = useTopSpendingByCategory();
-  const { data: monthlySummary, isLoading: isMonthlySummaryLoading } = useMonthlySummary();
-
-  if (isSpendingLoading
-    || isTopSpendingLoading
-    || isMonthlySummaryLoading) {
-    return <div>Loading...</div>;
-  }
-
-  console.log('all data', { spendingCategories, topSpending, monthlySummary });
-
   return (
     <div className="space-y-6">
       <Summary />
