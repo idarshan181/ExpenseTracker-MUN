bf76e50d78a4855530210e98c982c3af827a52a8
vtpatel <vivekpatel21101@gmail.com>
2025-03-23 03:02:59 -0230

    chore: add jest.setup.ts for Jest configuration

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,6 @@
 import '@testing-library/jest-dom';
+
+jest.mock('next/font/google', () => ({
+  Geist: () => ({ variable: '--font-geist-sans' }),
+  Geist_Mono: () => ({ variable: '--font-geist-mono' }),
+}));
